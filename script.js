37ef2a22ee6a9188ad4e7ed2d47e50ade701eddb
Servicios-RSA <92615260+Servicios-RSA@users.noreply.github.com>
2021-11-09 16:49:33 +0000

    Deploying to gh-pages from @ Servicios-RSA/FormularioRSA@3bb1cad79b2a7aa5117842a0bafd8ab5338f3061 🚀

--- script.js
+++ script.js
@@ -4,6 +4,6 @@ $form.addEventListener('sumbit', handleSumbit)
 function handleSumbit(event) {
     event.preventDefault()
     const form = new FormData(this)
-    $buttonMailto.setAttribute('href', `mailto:danielysolo@gmail.com?subject= nombre: ${form.get('Nombre')} correo: ${form.get('Email')}&body= ${form.get('Mensaje')} numero: ${form.get('Numero')}`)
+    $buttonMailto.setAttribute('href', `mailto:rsasas.bga@gmail.com?subject= nombre: ${form.get('Nombre')} correo: ${form.get('Email')}&body= ${form.get('Mensaje')} numero: ${form.get('Numero')}`)
     $buttonMailto.click()
 }
